8c6ebc058b9f2ef5fc65e9a831e779a8ab2f5fb7
Mantistc <omarmuhieddinedarwich@gmail.com>
2025-03-04 20:05:14 -0500

    adding tower types & some configs

--- src/tower_building/build.rs
+++ src/tower_building/build.rs
@@ -1,54 +1,28 @@
 use bevy::prelude::*;
 
-use super::{SPAWN_X_LOCATION, SPAWN_Y_LOCATION};
+use super::{TowerType, SPAWN_X_LOCATION, SPAWN_Y_LOCATION};
 
-#[derive(Component, Debug)]
-pub struct Tower {
+#[derive(Debug, Clone)]
+pub struct TowerInfo {
     pub attack_damage: u8,
     pub attack_speed: Timer,
+    pub level: u8,
+    pub tower_type: TowerType,
 }
+
+#[derive(Component, Debug, Deref, DerefMut)]
+pub struct Tower(pub TowerInfo);
+
 
 impl Default for Tower {
     fn default() -> Self {
-        Self {
+        Self(TowerInfo {
             attack_speed: Timer::from_seconds(0.25, TimerMode::Repeating),
             attack_damage: 5,
-        }
+            level: 1,
+            tower_type: TowerType::Lich,
+        })
     }
-}
-
-pub fn spawn_tower(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let texture = asset_server.load("towers/tower.png"); // simulate this is a tower
-    commands.spawn((
-        Sprite::from_image(texture.clone()),
-        Tower::default(),
-        Transform {
-            translation: Vec3::new(135.0, SPAWN_Y_LOCATION, 1.0),
-            scale: Vec3::splat(2.0),
-            ..default()
-        },
-    ));
-    commands.spawn((
-        Sprite::from_image(texture.clone()),
-        Tower::default(),
-        Transform {
-            translation: Vec3::new(SPAWN_X_LOCATION, SPAWN_Y_LOCATION, 1.0),
-            scale: Vec3::splat(2.0),
-            ..default()
-        },
-    ));
-    commands.spawn((
-        Sprite::from_image(texture.clone()),
-        Tower {
-            attack_damage: 10,
-            attack_speed: Timer::from_seconds(0.20, TimerMode::Repeating),
-        },
-        Transform {
-            translation: Vec3::new(-110.0, SPAWN_Y_LOCATION, 1.0),
-            scale: Vec3::splat(2.0),
-            ..default()
-        },
-    ));
 }
 
 pub fn track_cursor_position(
@@ -62,16 +36,18 @@ pub fn track_cursor_position(
             if let Ok(world_position) = camera.viewport_to_world(camera_transform, cursor_position)
             {
                 let world_coords = world_position.origin.truncate(); // Vec2
-                // info!("Cursor World Position: {:?}", world_coords);
+                                                                     // info!("Cursor World Position: {:?}", world_coords);
             }
         }
     }
 }
 
-pub fn check_click_in_area(
+pub fn click_and_spawn(
     windows: Query<&Window>,
     buttons: Res<ButtonInput<MouseButton>>,
     camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
 ) {
     let window = windows.single();
     let range = 16.0;
@@ -88,7 +64,16 @@ pub fn check_click_in_area(
                     && cursor_world_pos.y <= SPAWN_Y_LOCATION + range
                 {
                     if buttons.just_pressed(MouseButton::Left) {
-                        info!("Clicked inside the area: {:?}", cursor_world_pos);
+                        let texture = asset_server.load("towers/tower.png");
+                        commands.spawn((
+                            Sprite::from_image(texture.clone()),
+                            Tower::default(),
+                            Transform {
+                                translation: Vec3::new(SPAWN_X_LOCATION, SPAWN_Y_LOCATION, 1.0),
+                                scale: Vec3::splat(2.0),
+                                ..default()
+                            },
+                        ));
                     }
                 }
             }
--- src/tower_building/config.rs
+++ src/tower_building/config.rs
@@ -1,14 +1,10 @@
+use std::{ops::Deref, u8};
+
 use bevy::prelude::*;
 
 use super::{
-    check_click_in_area, shot_enemies, spawn_shots_to_attack, spawn_tower, track_cursor_position,
+    click_and_spawn, shot_enemies, spawn_shots_to_attack, track_cursor_position, TowerInfo,
 };
-
-#[derive(Resource, Debug)]
-pub struct Gold(pub u16);
-
-#[derive(Resource, Debug)]
-pub struct Lifes(pub u8);
 
 pub const SPAWN_Y_LOCATION: f32 = 25.0;
 pub const SPAWN_X_LOCATION: f32 = 15.0;
@@ -16,6 +12,7 @@ pub const TOWER_ATTACK_RANGE: f32 = 250.0;
 pub const DESPAWN_SHOT_RANGE: f32 = 800.0;
 pub const SHOT_HURT_DISTANCE: f32 = 700.0;
 pub const SHOT_SPEED: f32 = 700.0;
+pub const CONTROL: f32 = 0.5;
 
 pub struct TowersPlugin;
 
@@ -23,10 +20,78 @@ impl Plugin for TowersPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Gold(100))
             .insert_resource(Lifes(30))
-            .add_systems(Startup, spawn_tower)
             // build systems
-            .add_systems(Update, (track_cursor_position, check_click_in_area))
+            .add_systems(Update, (track_cursor_position, click_and_spawn))
             // attack systems
             .add_systems(Update, (spawn_shots_to_attack, shot_enemies));
     }
 }
+
+#[derive(Resource, Debug)]
+pub struct Gold(pub u16);
+
+#[derive(Resource, Debug)]
+pub struct Lifes(pub u8);
+
+#[derive(Resource, Debug)]
+pub struct TowerControl {
+    // with this we can crontrol if in a specific position there is already a tower placed
+    pub placements: [u8; TOWER_POSITION_PLACEMENT.len()],
+    pub textures: Vec<(Handle<Image>, Handle<TextureAtlasLayout>)>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum TowerType {
+    Lich,
+    Zigurat,
+    Electric,
+}
+
+impl TowerType {
+    pub fn to_cost(&self, lvl: u8) -> u8 {
+        const COST_TABLE: [[u8; 3]; 3] = [[5, 25, 50], [25, 75, 160], [50, 125, 225]];
+
+        let tower_index = match self {
+            TowerType::Lich => 0,
+            TowerType::Zigurat => 1,
+            TowerType::Electric => 2,
+        };
+
+        COST_TABLE[tower_index]
+            .get((lvl - 1) as usize)
+            .copied()
+            .unwrap_or(u8::MAX)
+    }
+
+    pub fn to_tower_data(&self, lvl: u8) -> TowerInfo {
+        let attack_speed = Timer::from_seconds(0.25, TimerMode::Repeating);
+
+        let attack_damage = match self {
+            TowerType::Lich => ((5 * lvl) as f32 * CONTROL).round().clamp(0.0, 255.0) as u8,
+            TowerType::Zigurat => ((8 * lvl) as f32 * CONTROL).round().clamp(0.0, 255.0) as u8,
+            TowerType::Electric => ((12 * lvl) as f32 * CONTROL).round().clamp(0.0, 255.0) as u8,
+        };
+
+        TowerInfo {
+            attack_speed,
+            attack_damage,
+            level: lvl,
+            tower_type: self.clone(),
+        }
+    }
+}
+pub const TOWER_POSITION_PLACEMENT: [Vec2; 13] = [
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+    Vec2::new(25.0, 15.0),
+];
